6cad8afb9f4b3873b21576dde554ffe8ecd0c723
yashanand167 <yash.anand167@gmail.com>
2026-08-19 18:19:24 +0530

    Update dependencies for better-auth and related packages
    - Added new dependencies: @better-auth/drizzle-adapter and better-auth, both at version 1.7.1.
    - Updated bun.lock and package.json to include the new dependencies, ensuring compatibility with the latest features and improvements.

---
+++ lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { nextCookies } from "better-auth/next-js";
+import db from "@/db";
+
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
